29b5003b0a27b02aa358d9dd565908753ea6a2f2
Gustavo <gustavods2003@outlook.com>
2025-03-10 17:53:04 -0300

    feat: interface criar e buscar usuario por email

--- src/application/use-cases/user/cadastro-usuario-use-case.ts
+++ src/application/use-cases/user/cadastro-usuario-use-case.ts
@@ -1,13 +1,21 @@
 import { ICadastroUsuarioUseCase } from '@/domain/use-cases';
+import {
+  ICreateUserRepository,
+  IFindUserByEmailRepository,
+} from '@/application/protocols/repositories';
 
 export class CadastroUsuarioUseCase implements ICadastroUsuarioUseCase {
-  constructor() {}
+  constructor(
+    private userRepo: ICreateUserRepository & IFindUserByEmailRepository,
+  ) {}
 
   async execute({
     email,
     name,
     senha,
   }: ICadastroUsuarioUseCase.Params): Promise<ICadastroUsuarioUseCase.Result> {
+    const existedUser = await this.userRepo.findByEmail(email);
+    if (existedUser) throw new Error('E-mail já cadastrado');
     throw new Error('Non implemented method');
   }
 }
