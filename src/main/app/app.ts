804ed8575ccc0d6a4820546da94fd4428e699d38
Gustavo <gustavods2003@outlook.com>
2025-03-11 10:49:09 -0300

    feat: setup middlewares app express

--- src/main/app/app.ts
+++ src/main/app/app.ts
@@ -1,6 +1,7 @@
-import { setupRoutes } from './config';
+import { setupMiddlewares, setupRoutes } from './config';
 import express from 'express';
 
 const app = express();
+setupMiddlewares(app);
 setupRoutes(app);
 export { app };
