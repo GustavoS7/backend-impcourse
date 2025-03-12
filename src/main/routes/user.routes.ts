c7f42a490549269a839debcca8321fa6d9ea5c34
Gustavo <gustavods2003@outlook.com>
2025-03-12 08:41:19 -0300

    feat: rota /login POST do usuario

--- src/main/routes/user.routes.ts
+++ src/main/routes/user.routes.ts
@@ -1,9 +1,13 @@
-import { makeCadastroUsuarioController } from '../factory/presentation/controllers';
 import { adaptExpressRoute as adapter } from '../adapters';
 import { Router } from 'express';
+import {
+  makeCadastroUsuarioController,
+  makeLoginUsuarioController,
+} from '../factory/presentation/controllers';
 
 const userRoutes = Router();
 
 userRoutes.post('/cadastro', adapter(makeCadastroUsuarioController()));
+userRoutes.post('/login', adapter(makeLoginUsuarioController()));
 
 export { userRoutes };
