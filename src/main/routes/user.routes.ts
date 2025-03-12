89ccd6fc1346205480cab0b671762b4a0da726fd
Gustavo <gustavods2003@outlook.com>
2025-03-12 09:21:18 -0300

    feat: rota /me GET para retornar usuario

--- src/main/routes/user.routes.ts
+++ src/main/routes/user.routes.ts
@@ -1,13 +1,23 @@
-import { adaptExpressRoute as adapter } from '../adapters';
+import { makeAuthMiddleware } from '../factory/presentation/middleware';
 import { Router } from 'express';
+import {
+  adaptExpressRoute as adapter,
+  adaptExpressMiddleware as adapterMiddleware,
+} from '../adapters';
 import {
   makeCadastroUsuarioController,
   makeLoginUsuarioController,
+  makeRetornarUsuarioController,
 } from '../factory/presentation/controllers';
 
 const userRoutes = Router();
 
 userRoutes.post('/cadastro', adapter(makeCadastroUsuarioController()));
 userRoutes.post('/login', adapter(makeLoginUsuarioController()));
+userRoutes.get(
+  '/me',
+  adapterMiddleware(makeAuthMiddleware()),
+  adapter(makeRetornarUsuarioController()),
+);
 
 export { userRoutes };
