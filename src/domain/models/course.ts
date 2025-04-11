120fa77942c20121dbbede83dd13dae5bc12a829
Gustavo <gustavods2003@outlook.com>
2025-04-11 12:09:26 -0300

    feat: Tipagem curso populado

--- src/domain/models/course.ts
+++ src/domain/models/course.ts
@@ -1,3 +1,19 @@
+import { Decimal } from '@prisma/client/runtime/library';
 import { Course } from '@prisma/client';
+import { TContent } from './content';
+import { TUser } from './user';
 
 export type TCourse = Course;
+
+export type TCoursePopulate = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  title: string;
+  description: string;
+  category: string | null;
+  price: Decimal;
+  authorId: string;
+  author: TUser;
+  content: TContent[];
+};
