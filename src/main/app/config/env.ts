68f4f76974a0ec7e1f90deee030019ad7243e265
Gustavo <gustavods2003@outlook.com>
2025-04-11 12:29:33 -0300

    chore: adicionar credenciais AWS ao env

--- src/main/app/config/env.ts
+++ src/main/app/config/env.ts
@@ -5,6 +5,10 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(8080),
   ACCESS_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_SECRET: z.string(),
+  AWS_REGION: z.string(),
+  AWS_ACCESS_KEY: z.string(),
+  AWS_SECRET_KEY: z.string(),
+  AWS_BUCKET_NAME: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
