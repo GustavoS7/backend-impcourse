5c54631cae539ef93704f79bd5e68c7d28e99a18
Gustavo <gustavods2003@outlook.com>
2025-04-11 12:36:18 -0300

    feat: implementar cadastro de aula controller

---
+++ src/presentation/controllers/content/cadastro-aula-controller.ts
@@ -0,0 +1,89 @@
+import { Controller, HttpResponse } from '@/presentation/protocols';
+import { ICadastroAulaUseCase } from '@/domain/use-cases';
+import { TContent } from '@/domain/models';
+import { z } from 'zod';
+
+type TCadastroAulaInput = {
+  body: { title: string; description: string; courseId: string };
+  user: { userId: string };
+  file: Express.Multer.File;
+};
+
+type TCadastroAulaOutput = TContent;
+
+export class CadastroAulaController extends Controller {
+  constructor(private readonly cadastroAulaUseCase: ICadastroAulaUseCase) {
+    super();
+  }
+
+  override async perform(
+    httpRequest: TCadastroAulaInput,
+  ): Promise<HttpResponse<TCadastroAulaOutput>> {
+    const { description, title, courseId } = httpRequest.body;
+    const { userId } = httpRequest.user;
+    const { file } = httpRequest;
+    const response = await this.cadastroAulaUseCase.execute({
+      file,
+      description,
+      title,
+      courseId,
+      userId,
+    });
+    return {
+      statusCode: 201,
+      payload: response,
+    };
+  }
+
+  override validationSchema() {
+    return {
+      body: z.object({
+        title: z
+          .string({
+            required_error: 'Campo title precisa ser especificado',
+            invalid_type_error: 'Campo title precisa ser especificado',
+          })
+          .min(1, { message: 'Campo title precisa ter pelo menos 1 caracter' }),
+        description: z
+          .string({
+            required_error: 'Campo description precisa ser especificado',
+            invalid_type_error: 'Campo description precisa ser especificado',
+          })
+          .min(1, {
+            message: 'Campo description precisa ter pelo menos 1 caracter',
+          }),
+        courseId: z
+          .string({
+            required_error: 'Campo description precisa ser especificado',
+            invalid_type_error: 'Campo description precisa ser especificado',
+          })
+          .min(1, {
+            message: 'Campo description precisa ter pelo menos 1 caracter',
+          }),
+      }),
+      user: z.object({
+        userId: z
+          .string({
+            required_error: 'Campo title precisa ser especificado',
+            invalid_type_error: 'Campo title precisa ser especificado',
+          })
+          .min(1, { message: 'Campo title precisa ter pelo menos 1 caracter' }),
+      }),
+      file: z.custom<Express.Multer.File>(
+        (file) => {
+          if (!file) return false;
+
+          const allowedTypes = ['image/jpeg', 'image/png', 'video/mp4'];
+          const maxSizeInBytes = 10 * 1024 * 1024;
+
+          return (
+            allowedTypes.includes(file.mimetype) && file.size <= maxSizeInBytes
+          );
+        },
+        {
+          message: 'Arquivo inválido. Formato ou tamanho não permitido.',
+        },
+      ),
+    };
+  }
+}
