4a3f629bbff4ff6e0656d82d3fa51301bf2b356d
Gustavo <gustavods2003@outlook.com>
2025-04-11 12:08:48 -0300

    feat: implementar get course

--- src/infrastructure/repositories/prisma/prisma-course-repository.ts
+++ src/infrastructure/repositories/prisma/prisma-course-repository.ts
@@ -1,16 +1,58 @@
 import { RepositoryError } from '@/application/errors';
 import { prisma } from '@/infrastructure/lib';
-import { ICreateCourseRepository } from '@/application/protocols/repositories';
+import {
+  ICreateCourseRepository,
+  IGetCoursePopulateRepository,
+  IGetCourseRepository,
+} from '@/application/protocols/repositories';
 
-export class PrismaCourseRepository implements ICreateCourseRepository {
+export class PrismaCourseRepository
+  implements
+    ICreateCourseRepository,
+    IGetCourseRepository,
+    IGetCoursePopulateRepository
+{
   async create(
     data: ICreateCourseRepository.Params,
   ): Promise<ICreateCourseRepository.Result> {
     try {
-      const user = await prisma.course.create({ data });
-      return user;
+      const course = await prisma.course.create({ data });
+      return course;
     } catch (error) {
-      console.log(error);
+      throw new RepositoryError();
+    }
+  }
+
+  async get(id: string): Promise<IGetCourseRepository.Result> {
+    try {
+      const course = await prisma.course.findUnique({
+        where: {
+          id,
+        },
+      });
+      return course;
+    } catch (error) {
+      throw new RepositoryError();
+    }
+  }
+
+  async getPopulate(id: string): Promise<IGetCoursePopulateRepository.Result> {
+    try {
+      const course = await prisma.course.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          author: true,
+          content: {
+            orderBy: {
+              position: 'desc',
+            },
+          },
+        },
+      });
+      return course;
+    } catch (error) {
       throw new RepositoryError();
     }
   }
