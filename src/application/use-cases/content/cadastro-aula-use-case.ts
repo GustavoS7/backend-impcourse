bdda628344888be31d5c763520e2df31f2473011
Gustavo <gustavods2003@outlook.com>
2025-04-11 12:09:07 -0300

    feat: implementar cadastro aula

--- src/application/use-cases/content/cadastro-aula-use-case.ts
+++ src/application/use-cases/content/cadastro-aula-use-case.ts
@@ -1,23 +1,38 @@
-import { ICreateCourseRepository } from '@/application/protocols/repositories';
-import { ICadastroCursoUseCase } from '@/domain/use-cases';
+import { IUploadFileProvider } from '@/application/protocols/providers';
+import { ICadastroAulaUseCase } from '@/domain/use-cases';
+import { BadRequestError } from '@/application/errors';
+import {
+  ICreateContentRepository,
+  IGetCoursePopulateRepository,
+} from '@/application/protocols/repositories';
 
-export class CadastroCursoUseCase implements ICadastroCursoUseCase {
-  constructor(private courseRepository: ICreateCourseRepository) {}
+export class CadastroAulaUseCase implements ICadastroAulaUseCase {
+  constructor(
+    private courseRepository: IGetCoursePopulateRepository,
+    private storageProvider: IUploadFileProvider,
+    private contentRepository: ICreateContentRepository,
+  ) {}
 
   async execute({
-    category,
-    description,
-    price,
     title,
+    file,
+    description,
+    courseId,
     userId,
-  }: ICadastroCursoUseCase.Params): Promise<ICadastroCursoUseCase.Result> {
-    const course = await this.courseRepository.create({
-      authorId: userId,
-      category,
-      description,
-      price,
+  }: ICadastroAulaUseCase.Params): Promise<ICadastroAulaUseCase.Result> {
+    const course = await this.courseRepository.getPopulate(courseId);
+    if (!course || course?.authorId !== userId)
+      throw new BadRequestError('Curso não encontrado');
+    const url = await this.storageProvider.upload(file);
+    const lastLesson = course.content[0];
+    const content = await this.contentRepository.create({
       title,
+      description,
+      type: file.mimetype,
+      url,
+      courseId,
+      position: lastLesson ? lastLesson.position + 1 : 1,
     });
-    return course;
+    return content;
   }
 }
